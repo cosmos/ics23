cf992fd279445556c72f21b16d716d1e260ab9ea
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-04-18 21:56:48 +0200

    Improve test setup

--- js/jasmine-testrunner.js
+++ js/jasmine-testrunner.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-// require('source-map-support').install();
+require('source-map-support').install();
 
 // setup Jasmine
 const Jasmine = require('jasmine');
@@ -16,17 +16,17 @@ jasmine.loadConfig({
 jasmine.jasmine.DEFAULT_TIMEOUT_INTERVAL = 15*1000;
 
 // setup console reporter
-// const JasmineConsoleReporter = require('jasmine-console-reporter');
-// const consoleReporter = new JasmineConsoleReporter({
-//   colors: 1,           // (0|false)|(1|true)|2
-//   cleanStack: 1,       // (0|false)|(1|true)|2|3
-//   verbosity: 4,        // (0|false)|1|2|(3|true)|4
-//   listStyle: 'indent', // "flat"|"indent"
-//   activity: true,
-//   emoji: true,
-// });
+const JasmineConsoleReporter = require('jasmine-console-reporter');
+const consoleReporter = new JasmineConsoleReporter({
+  colors: 1,           // (0|false)|(1|true)|2
+  cleanStack: 1,       // (0|false)|(1|true)|2|3
+  verbosity: 4,        // (0|false)|1|2|(3|true)|4
+  listStyle: 'indent', // "flat"|"indent"
+  activity: true,
+  emoji: true,
+});
 
 // // initialize and execute
-// jasmine.env.clearReporters();
-// jasmine.addReporter(consoleReporter);
+jasmine.env.clearReporters();
+jasmine.addReporter(consoleReporter);
 jasmine.execute();
