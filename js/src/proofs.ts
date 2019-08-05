6b98e5a0e720939e8e42fcbffc513c66076feb5b
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-08-05 15:55:53 +0200

    Add ensureSpec to proofs.ts

--- js/src/proofs.ts
+++ js/src/proofs.ts
@@ -21,3 +21,67 @@ export function calculateExistenceRoot(
   }
   return res;
 }
+
+// ensureSpec throws an Error if proof doesn't fulfill spec
+export function ensureSpec(
+  proof: proofs.IExistenceProof,
+  spec: proofs.IProofSpec
+): void {
+  if (!proof.leaf) {
+    throw new Error("Existence proof must start with a leaf operation");
+  }
+  if (!spec.leafSpec) {
+    throw new Error("Spec must include leafSpec");
+  }
+  ensureLeaf(proof.leaf, spec.leafSpec);
+  const path = proof.path || [];
+  for (const inner of path) {
+    ensureInner(inner, spec.leafSpec.prefix);
+  }
+}
+
+function ensureLeaf(leaf: proofs.ILeafOp, spec: proofs.ILeafOp): void {
+  if (leaf.hash !== spec.hash) {
+    throw new Error(`Unexpected hashOp: ${leaf.hash}`);
+  }
+  if (leaf.prehashKey !== spec.prehashKey) {
+    throw new Error(`Unexpected prehashKey: ${leaf.prehashKey}`);
+  }
+  if (leaf.prehashValue !== spec.prehashValue) {
+    throw new Error(`Unexpected prehashValue: ${leaf.prehashValue}`);
+  }
+  if (leaf.length !== spec.length) {
+    throw new Error(`Unexpected length op: ${leaf.length}`);
+  }
+  ensurePrefix(leaf.prefix, spec.prefix);
+}
+
+function ensureInner(inner: proofs.IInnerOp, prefix?: Uint8Array | null): void {
+  ensurePrefix(inner.prefix, prefix);
+}
+
+function ensurePrefix(
+  check?: Uint8Array | null,
+  prefix?: Uint8Array | null
+): void {
+  // no prefix supplied, means everything passes
+  if (!prefix || prefix.length === 0) {
+    return;
+  }
+  if (!check) {
+    throw new Error(`Target bytes missing`);
+  }
+  ensureBytesEqual(prefix, check.slice(0, prefix.length));
+}
+
+// ensureBytesEqual throws an error if the arrays are different
+function ensureBytesEqual(a: Uint8Array, b: Uint8Array): void {
+  if (a.length !== b.length) {
+    throw new Error(`Different lengths ${a.length} vs ${b.length}`);
+  }
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      throw new Error(`Arrays differ at index ${i}: ${a[i]} vs ${b[i]}`);
+    }
+  }
+}
