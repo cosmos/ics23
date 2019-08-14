b5cbf9dfc009648e3b9fb1b61065a48451ac613f
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-08-14 15:02:07 +0200

    Test apply_inner

--- rust/src/ops.rs
+++ rust/src/ops.rs
@@ -83,7 +83,6 @@ fn proto_len(length: usize) -> Hash {
 #[cfg(test)]
 mod tests {
     use super::*;
-    // use protobuf::Message;
 
     #[test]
     fn it_works() {
@@ -160,4 +159,41 @@ mod tests {
         assert_eq!(hash, apply_leaf(leaf, key, val).unwrap());
     }
 
+    #[test]
+    fn apply_inner_prefix_suffix() {
+        let mut inner = InnerOp::new();
+        inner.set_hash(HashOp::SHA256);
+        inner.set_prefix(hex::decode("0123456789").unwrap());
+        inner.set_suffix(hex::decode("deadbeef").unwrap());
+        let child = hex::decode("00cafe00").unwrap();
+
+        // echo -n 012345678900cafe00deadbeef | xxd -r -p | sha256sum
+        let expected = hex::decode("0339f76086684506a6d42a60da4b5a719febd4d96d8b8d85ae92849e3a849a5e").unwrap();
+        assert_eq!(expected, apply_inner(inner, child).unwrap());
+    }
+
+    #[test]
+    fn apply_inner_prefix_only() {
+        let mut inner = InnerOp::new();
+        inner.set_hash(HashOp::SHA256);
+        inner.set_prefix(hex::decode("00204080a0c0e0").unwrap());
+        let child = hex::decode("ffccbb997755331100").unwrap();
+
+        // echo -n 00204080a0c0e0ffccbb997755331100 | xxd -r -p | sha256sum
+        let expected = hex::decode("45bece1678cf2e9f4f2ae033e546fc35a2081b2415edcb13121a0e908dca1927").unwrap();
+        assert_eq!(expected, apply_inner(inner, child).unwrap());
+    }
+
+
+    #[test]
+    fn apply_inner_suffix_only() {
+        let mut inner = InnerOp::new();
+        inner.set_hash(HashOp::SHA256);
+        inner.set_suffix(Hash::from(" just kidding!"));
+        let child = Hash::from("this is a sha256 hash, really....");
+
+        // echo -n 'this is a sha256 hash, really.... just kidding!'  | sha256sum
+        let expected = hex::decode("79ef671d27e42a53fba2201c1bbc529a099af578ee8a38df140795db0ae2184b").unwrap();
+        assert_eq!(expected, apply_inner(inner, child).unwrap());
+    }
 }
