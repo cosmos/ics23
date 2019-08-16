65916242a769f374a2fbd1a802ccd82d058501e6
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-08-16 16:34:40 +0200

    Updated typescript vector tests

--- js/src/testvectors.spec.ts
+++ js/src/testvectors.spec.ts
@@ -2,55 +2,54 @@ import { readFileSync } from "fs";
 
 import { proofs } from "./generated/codecimpl";
 import { fromHex } from "./helpers";
-import { IavlSpec, TendermintSpec, verifyExistence } from "./proofs";
+import { verifyMembership } from "./ics23";
+import { IavlSpec, TendermintSpec } from "./proofs";
 
 describe("calculateExistenceRoot", () => {
   function validateTestVector(filepath: string, spec: proofs.IProofSpec): void {
     const content = readFileSync(filepath).toString();
-    const { root, existence } = JSON.parse(content);
-    expect(existence).toBeDefined();
+    const { root, proof, key, value } = JSON.parse(content);
+    expect(proof).toBeDefined();
     expect(root).toBeDefined();
+    expect(key).toBeDefined();
 
-    const proof = proofs.ExistenceProof.decode(fromHex(existence));
     const rootHash = fromHex(root);
+    const bkey = fromHex(key);
+    const commit = proofs.CommitmentProof.decode(fromHex(proof));
 
-    verifyExistence(proof, spec, rootHash, proof.key, proof.value);
+    if (value) {
+      const bvalue = fromHex(value);
+      verifyMembership(commit, spec, rootHash, bkey, bvalue);
+    } else {
+      throw new Error("non membership not yet implemented");
+    }
   }
 
   it("should parse iavl 1", () => {
-    validateTestVector("../testdata/iavl/existence1.json", IavlSpec);
+    validateTestVector("../testdata/iavl/exist_left.json", IavlSpec);
   });
   it("should parse iavl 2", () => {
-    validateTestVector("../testdata/iavl/existence2.json", IavlSpec);
+    validateTestVector("../testdata/iavl/exist_right.json", IavlSpec);
   });
   it("should parse iavl 3", () => {
-    validateTestVector("../testdata/iavl/existence3.json", IavlSpec);
-  });
-  it("should parse iavl 4", () => {
-    validateTestVector("../testdata/iavl/existence4.json", IavlSpec);
+    validateTestVector("../testdata/iavl/exist_middle.json", IavlSpec);
   });
 
   it("should parse tendermint 1", () => {
     validateTestVector(
-      "../testdata/tendermint/existence1.json",
+      "../testdata/tendermint/exist_left.json",
       TendermintSpec
     );
   });
   it("should parse tendermint 2", () => {
     validateTestVector(
-      "../testdata/tendermint/existence2.json",
+      "../testdata/tendermint/exist_right.json",
       TendermintSpec
     );
   });
   it("should parse tendermint 3", () => {
     validateTestVector(
-      "../testdata/tendermint/existence3.json",
-      TendermintSpec
-    );
-  });
-  it("should parse tendermint 4", () => {
-    validateTestVector(
-      "../testdata/tendermint/existence4.json",
+      "../testdata/tendermint/exist_middle.json",
       TendermintSpec
     );
   });
