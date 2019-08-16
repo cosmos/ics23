c8047ad535f36a2b581b069ba75afee0142f2370
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-08-16 18:07:58 +0200

    Validate left-most and right-most checks for non-existence

--- js/src/proofs.ts
+++ js/src/proofs.ts
@@ -14,6 +14,12 @@ export const IavlSpec: proofs.IProofSpec = {
     prehashValue: proofs.HashOp.SHA256,
     prehashKey: proofs.HashOp.NO_HASH,
     length: proofs.LengthOp.VAR_PROTO
+  },
+  innerSpec: {
+    childOrder: [0, 1],
+    minPrefixLength: 4,
+    maxPrefixLength: 12,
+    childSize: 33
   }
 };
 
@@ -24,6 +30,12 @@ export const TendermintSpec: proofs.IProofSpec = {
     prehashValue: proofs.HashOp.SHA256,
     prehashKey: proofs.HashOp.NO_HASH,
     length: proofs.LengthOp.VAR_PROTO
+  },
+  innerSpec: {
+    childOrder: [0, 1],
+    minPrefixLength: 1,
+    maxPrefixLength: 1,
+    childSize: 32
   }
 };
 
@@ -83,18 +95,86 @@ export function verifyNonExistence(
     ensureBytesBefore(key, rightKey);
   }
 
+  if (!spec.innerSpec) {
+    throw new Error("");
+  }
   if (!leftKey) {
-    // TODO: ensure right proof is left most
-    return;
+    ensureLeftMost(spec.innerSpec, proof.right!.path!);
+  } else if (!rightKey) {
+    ensureRightMost(spec.innerSpec, proof.left!.path!);
+  } else {
+    // TODO: ensure left and right are neighbors
   }
-
-  if (!rightKey) {
-    // TODO: ensure left proof is right most
-    return;
-  }
-
-  // TODO: ensure left and right are neighbors
   return;
+}
+
+export function ensureLeftMost(
+  spec: proofs.IInnerSpec,
+  path: ReadonlyArray<proofs.IInnerOp>
+): void {
+  const { minPrefix, maxPrefix, suffix } = getPadding(spec, 0);
+
+  // ensure every step has a prefix and suffix defined to be leftmost
+  for (const step of path) {
+    if (!hasPadding(step, minPrefix, maxPrefix, suffix)) {
+      throw new Error("Step not leftmost");
+    }
+  }
+}
+
+export function ensureRightMost(
+  spec: proofs.IInnerSpec,
+  path: ReadonlyArray<proofs.IInnerOp>
+): void {
+  const len = spec.childOrder!.length - 1;
+  const { minPrefix, maxPrefix, suffix } = getPadding(spec, len);
+
+  // ensure every step has a prefix and suffix defined to be leftmost
+  for (const step of path) {
+    if (!hasPadding(step, minPrefix, maxPrefix, suffix)) {
+      throw new Error("Step not leftmost");
+    }
+  }
+}
+
+function hasPadding(
+  op: proofs.IInnerOp,
+  minPrefix: number,
+  maxPrefix: number,
+  suffix: number
+): boolean {
+  if ((op.prefix || []).length < minPrefix) {
+    return false;
+  }
+  if ((op.prefix || []).length > maxPrefix) {
+    return false;
+  }
+  return (op.suffix || []).length === suffix;
+}
+
+interface PaddingResult {
+  readonly minPrefix: number;
+  readonly maxPrefix: number;
+  readonly suffix: number;
+}
+function getPadding(spec: proofs.IInnerSpec, branch: number): PaddingResult {
+  const idx = getPosition(spec.childOrder!, branch);
+
+  // count how many children are in the prefix
+  const prefix = idx * spec.childSize!;
+  const minPrefix = prefix + spec.minPrefixLength!;
+  const maxPrefix = prefix + spec.maxPrefixLength!;
+
+  // count how many children are in the suffix
+  const suffix = (spec.childOrder!.length - 1 - idx) * spec.childSize!;
+  return { minPrefix, maxPrefix, suffix };
+}
+
+function getPosition(order: ReadonlyArray<number>, branch: number): number {
+  if (branch < 0 || branch >= order.length) {
+    throw new Error(`Invalid branch: ${branch}`);
+  }
+  return order.findIndex(val => val === branch);
 }
 
 // Calculate determines the root hash that matches the given proof.
