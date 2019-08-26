4f0829b025669f95277033b6e75b147e7599cafc
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-08-26 12:58:01 +0200

    rust: Fix import error with new name

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,5 +6,5 @@ mod verify;
 
 pub use crate::proofs::*;
 pub use helpers::{Hash, Result};
-pub use ics23::{iavl_spec, tendermint_spec, verify_membership, verify_non_membership};
+pub use crate::ics23::{iavl_spec, tendermint_spec, verify_membership, verify_non_membership};
 pub use verify::calculate_existence_root;
