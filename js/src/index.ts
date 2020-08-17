e2004dc12bfeeda1088f6b47d13dc5d19bb94910
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-08-18 00:05:57 +0200

    Export iavl and tendermint specs

--- js/src/index.ts
+++ js/src/index.ts
@@ -3,6 +3,8 @@ export { verifyMembership, verifyNonMembership } from "./ics23";
 export {
   calculateExistenceRoot,
   CommitmentRoot,
+  iavlSpec,
+  tendermintSpec,
   verifyExistence,
   verifyNonExistence
 } from "./proofs";
