9877da2d31854bcbe6c41f8f08ea8232adb297a8
Marko Baricevic <markobaricevic3778@gmail.com>
2022-11-30 14:08:40 +0100

    add buf

---
+++ scripts/protogenc_go.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Generating gogo proto code"
+cd proto
+proto_dirs=$(find ./cosmos -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+for dir in $proto_dirs; do
+  for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
+      buf generate --template buf.gen.gogo.yaml $file
+  done
+done
+
+cd ..
+
+cp -r github.com/cosmos/ics23/* ./go
+rm -rf github.com
