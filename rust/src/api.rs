c9a1984b8bf501afd4f9b18dd00d6c215905039e
finch <finch@plaidfinch.net>
2023-03-28 17:09:50 -0400

    fix: Make it compile in no_std by removing naming of Vec

--- rust/src/api.rs
+++ rust/src/api.rs
@@ -141,14 +141,12 @@ fn get_nonexist_proof<'a, H: HostFunctionsProvider>(
     key: &[u8],
     spec: &ics23::ProofSpec,
 ) -> Option<&'a ics23::NonExistenceProof> {
-    let key_for_comparison = |key: &[u8]| -> Vec<u8> {
-        match spec.prehash_key_before_comparison {
-            true => do_hash::<H>(
-                spec.leaf_spec.clone().unwrap_or_default().prehash_key(),
-                key,
-            ),
-            false => key.to_vec(),
-        }
+    let key_for_comparison = |key: &[u8]| match spec.prehash_key_before_comparison {
+        true => do_hash::<H>(
+            spec.leaf_spec.clone().unwrap_or_default().prehash_key(),
+            key,
+        ),
+        false => key.to_vec(),
     };
 
     match &proof.proof {
