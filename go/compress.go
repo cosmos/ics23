cosmos/ics23

commit daa1760cb80f8607494ecf9e40482e66717a24e0

refactor: modified the code for compressStep function as per instructions (#149)

go/compress.go
@@ -93,17 +93,16 @@ func compressStep(step *InnerOp, lookup *[]*InnerOp, registry map[string]int32) 
 	if err != nil {
 		panic(err)
 	}
-	sig := string(bz)
 
 	// load from cache if there
-	if num, ok := registry[sig]; ok {
+        if num, ok := registry[string(bz)]; ok {
 		return num
 	}
 
 	// create new step if not there
 	num := int32(len(*lookup))
 	*lookup = append(*lookup, step)
-	registry[sig] = num
+        registry[string(bz)] = num	
 	return num
 }
 

commit ba2a773a136eb235e9bc3f290c1982332cabf84b

chore: fix lint issues introduced in #149

go/compress.go
@@ -95,14 +95,14 @@ func compressStep(step *InnerOp, lookup *[]*InnerOp, registry map[string]int32) 
 	}
 
 	// load from cache if there
-        if num, ok := registry[string(bz)]; ok {
+	if num, ok := registry[string(bz)]; ok {
 		return num
 	}
 
 	// create new step if not there
 	num := int32(len(*lookup))
 	*lookup = append(*lookup, step)
-        registry[string(bz)] = num	
+	registry[string(bz)] = num
 	return num
 }
 