d137921f67b68226cd09316aee9cf37321ebece1
DimitrisJim <d.f.hilliard@gmail.com>
2024-04-17 19:22:22 +0300

    feat: add api folder containing pulsar generated code.

---
+++ go/api/go.mod
@@ -0,0 +1,8 @@
+module github.com/cosmos/ics23/go/api
+
+go 1.22.0
+
+require (
+	github.com/cosmos/cosmos-proto v1.0.0-beta.4
+	google.golang.org/protobuf v1.33.0
+)
--- scripts/protocgen_go.sh
+++ scripts/protocgen_go.sh
@@ -15,3 +15,16 @@ cd ..
 
 cp -r github.com/cosmos/ics23/go/* ./go
 rm -rf github.com
+
+echo "Cleaning API directory"
+(cd go/api; find ./ -type f \( -iname \*.pulsar.go -o -iname \*.pb.go -o -iname \*.pb.gw.go \) -delete; find . -empty -type d -delete; cd ../..)
+
+echo "Generating API module"
+(cd proto; buf generate --template buf.gen.pulsar.yaml)
+
+# move files to appropriate location (go/api)
+cp -r go/api/github.com/cosmos/ics23/go/* go/api/
+rm -rf go/api/github.com
+
+# tidy go/api module
+(cd go/api; go mod tidy)
